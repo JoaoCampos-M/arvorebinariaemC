759e15cf3d00b6378be0d41656cf5ddeec690cf8
joao <joao.campos@unemat.br>
2023-05-13 19:53:55 -0400

    finaliza exercicio 1

--- ArvoreBinaria.c
+++ ArvoreBinaria.c
@@ -22,15 +22,54 @@ int conta_nos(struct No* no) {
         return 1 + conta_nos(no->esq) + conta_nos(no->dir);
 }
 
+int conta_nos_nao_folha(struct No* no) {
+    if (no == NULL || (no->esq == NULL && no->dir == NULL))
+        return 0;
+    else
+        return 1 + conta_nos_nao_folha(no->esq) + conta_nos_nao_folha(no->dir);
+}
+
+int altura(struct No* no) {
+    if (no == NULL)
+        return -1;
+    else {
+        int esq_altura = altura(no->esq);
+        int dir_altura = altura(no->dir);
+        if (esq_altura > dir_altura)
+            return esq_altura + 1;
+        else
+            return dir_altura + 1;
+    }
+}
+
+int soma_arvore(struct No* no) {
+    if (no == NULL)
+        return 0;
+    else
+        return no->value + soma_arvore(no->esq) + soma_arvore(no->dir);
+}
+
+float media(struct No* no) {
+    int num_nos = conta_nos(no);
+    if (num_nos == 0)
+        return 0;
+    else
+        return (float) soma_arvore(no) / num_nos;
+}
+
 int main() {
     // Exemplo de uso
     struct No* raiz = cria_no(1);
-    raiz->esq = cria_no(2);
-    raiz->dir = cria_no(3);
-    raiz->esq->esq = cria_no(4);
-    raiz->esq->dir = cria_no(5);
+    raiz->esq = cria_no(6);
+    raiz->dir = cria_no(13);
+    raiz->esq->esq = cria_no(8);
+    raiz->esq->dir = cria_no(3);
 
-    printf("%d", conta_nos(raiz)); // saída: 5
+    printf("a quantidade de nós é %d", conta_nos(raiz)); // saída: 5
+    printf("\n a quantidade de nós não folha é: %d", conta_nos_nao_folha(raiz)); // saída: 5
+    printf("\n a altura é: %d", altura(raiz)); 
+    printf("\n a soma dos nós é: %d", soma_arvore(raiz)); 
+    printf("\n a média dos nós é: %.2f", media(raiz)); 
 
     return 0;
 }
